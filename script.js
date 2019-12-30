929a5396b69a367dcaed40530946abb174f1721a
jogindrakumar <joginder9868@outlook.com>
2019-12-30 11:49:40 +0530

    version 1.o

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+	var img1 = "url('https://cdn.pixabay.com/photo/2019/12/10/06/39/night-4685147_960_720.jpg')";
+	var img2 = "url('https://cdn.pixabay.com/photo/2019/11/27/12/37/autumn-4656877_960_720.jpg')";
+	var img3 = "url('https://cdn.pixabay.com/photo/2015/05/31/11/25/desk-791178_960_720.jpg')";
+	var img4 = "url('https://cdn.pixabay.com/photo/2019/12/11/17/34/camera-4688838_960_720.jpg')";
+	var img5 = "url('https://cdn.pixabay.com/photo/2015/04/20/13/17/work-731198_960_720.jpg')";
+	var img6 = "url('https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_960_720.jpg')";
+	var img7 = "url('https://cdn.pixabay.com/photo/2017/06/26/08/13/mockup-2443050_960_720.jpg')";
+	var img8 = "url('https://cdn.pixabay.com/photo/2017/07/31/11/44/laptop-2557576_960_720.jpg')";
+	var img9 = "url('https://cdn.pixabay.com/photo/2016/03/26/13/09/cup-of-coffee-1280537_960_720.jpg')";
+	var img10 = "url('https://cdn.pixabay.com/photo/2016/03/26/13/09/notebook-1280538_960_720.jpg')";
+
+
+
+	btn1.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage = img1;
+	})
+	btn2.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage =img2;
+	})
+	btn3.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage = img3;
+	})
+	btn4.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage =img4;
+	})
+	btn5.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage = img5;
+	})
+	btn6.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage =img6;
+	})
+	btn7.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage = img7;
+	})
+	btn8.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage =img8;
+	})
+	btn9.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage = img9;
+	})
+	btn10.addEventListener("click",function(){
+		document.getElementById("back").style.backgroundImage =img10;
+	})
